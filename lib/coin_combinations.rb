04555361297f7aca246939d68e8b18358a307929
Julia Dickey and Katarina Tuttle <katarina@>
2016-06-30 12:10:27 -0700

    Add nickel in specs and lib

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,13 +2,12 @@ require('pry')
 
 class Float
   define_method(:change) do
-    change = {"quarters" => 0, "dimes" => 0}
+    change = {"quarters" => 0, "dimes" => 0, "nickels" => 0}
     counter = 0
     input = self
     until counter.==(self)
       if input./(0.25) >= 1
         change["quarters"]=((input./(0.25)).floor())
-        # change.store("quarters", ((input./(0.25)).floor()))
         num_quarters = change.fetch("quarters")
         input = (input.-(0.25.*(num_quarters))).round(2)
         counter = (counter.+(0.25.*(num_quarters)))
@@ -17,6 +16,11 @@ class Float
         num_dimes = change.fetch("dimes")
         input = (input.-(0.10.*(num_dimes))).round(2)
         counter = (counter.+(0.10.*(num_dimes)))
+      elsif input./(0.05) >= 1
+        change["nickels"]=((input./(0.05)).floor())
+        num_nickels = change.fetch("nickels")
+        input = (input.-(0.05.*(num_nickels))).round(2)
+        counter = (counter.+(0.05.*(num_nickels)))
         # binding.pry
       else
         false
@@ -42,7 +46,34 @@ class Float
       result_dime = ""
     end
 
-    final_change = result_quarter.concat(" " + result_dime)
+    result_nickel = change.fetch("nickels")
+    if result_nickel > 1
+      result_nickel = (result_nickel.to_s()).concat(" nickels")
+    elsif result_nickel == 1
+      result_nickel = result_nickel.to_s().concat(" nickel")
+    else
+      result_nickel = ""
+    end
+
+    if (result_quarter != "") && (result_dime != "") && (result_nickel != "")
+      final_change = result_quarter.concat(" " + result_dime + " " + result_nickel)
+
+    elsif (result_quarter != "") && (result_nickel != "")
+      final_change = result_quarter.concat(" " + result_nickel)
+
+
+    elsif (result_dime != "") && (result_nickel != "")
+      final_change = result_dime.concat(" " + result_nickel)
+    elsif (result_dime != "") && (result_quarter != "")
+      final_change = result_quarter.concat(" " + result_dime)
+
+    elsif (result_quarter != "")
+      final_change = result_quarter
+    elsif (result_dime != "")
+      final_change = result_dime
+    elsif (result_nickel != "")
+      final_change = result_nickel
+    end
     final_change
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,9 +5,12 @@ require('capybara/rspec')
 
 describe('Float#change') do
   it("takes an amount and returns change in quarters") do
-    expect((0.75).change()).to(eq("3 quarters "))
+    expect((0.75).change()).to(eq("3 quarters"))
   end
   it("takes an amount and returns change in quarters and dimes") do
     expect((0.85).change()).to(eq("3 quarters 1 dime"))
+  end
+  it("takes an amount and returns change in quarters, dimes and nickles") do
+    expect((0.90).change()).to(eq("3 quarters 1 dime 1 nickel"))
   end
 end
